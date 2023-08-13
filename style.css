374f279d6edfd28117549ad399f1944aea65d44a
Ishan Bhat <124944324+asmartpersonwhoissmart@users.noreply.github.com>
2023-08-12 18:02:25 -0700

    Add files via upload

---
+++ style.css
@@ -0,0 +1,35 @@
+body{
+    background: #E2E8DD;
+    font-family: 'Akaya Telivigala', cursive;
+}
+.header_div{
+    background: #95A3A4;
+    color: white;
+}
+.header_div p{
+    font-size: 12px;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    letter-spacing: 2px;
+}
+.main_div button{
+    margin-top: 30px;
+    font-family: 'Roboto', sans-serif;
+    letter-spacing: 3px;
+    font-weight: bolder;
+    width: 130px;
+}
+.footer{
+    color: #6c757d;
+    font-size: 18px;
+    text-align: center;
+    width: 100%;
+    position: fixed;
+    bottom: 0px;
+    background-color: #f5f5f5;
+    line-height: 3;
+    text-shadow: 5px 5px 5px rgb(151,149,149);
+}
+h4{
+    font-size: 25px;
+}
